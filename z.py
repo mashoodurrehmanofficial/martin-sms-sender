30743291d01130a834096dec8fecbb6dce6a76d8
unknown <mashoodurrehmanofficial.com>
2022-07-25 10:52:53 +0500

    latest

--- z.py
+++ z.py
@@ -1,14 +1,1 @@
-import os,sys,subprocess
-
-file_path = os.path.join(os.getcwd(),"dist","QuickSMS.exe") 
-# os.startfile(file_path)
-# file_path = file_path.replace("\\", "\\\\")
-
-print("file_path = ",file_path)  
-
-o = subprocess.Popen([file_path, "--ask_product_key=False"],shell=True)
-# o = subprocess.Popen([file_path,],shell=True)
-# o = subprocess.Popen(["main.py", "--ask_product_key=False"],shell=True)
-
-# o.wait()  
-
+from textmagic.rest import TextmagicRestClient
